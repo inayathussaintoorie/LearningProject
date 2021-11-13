0fb121e6bbd8b9a09f282a839ffd917c3decd8da
inayathussaintoorie <inayathussaintoorie@gmail.com>
2021-11-13 14:50:08 +0500

    Add test project

---
+++ MyTestAPI/MyTestAPI.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace MyTestAPI.Tests.Controllers
+{
+    public class HomeControllerTestss
+    {
+        [Fact]
+        public void Index()
+        {
+            Assert.True(true);
+        }
+    }
+}
